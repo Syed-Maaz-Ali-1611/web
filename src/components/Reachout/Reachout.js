01b5ee5de708c288a50d8f8b9a916e92eacc6945
Syed-Maaz-Ali-1611 <72220673+Syed-Maaz-Ali-1611@users.noreply.github.com>
2024-07-16 23:16:33 +0500

    add calendly css

--- src/components/Reachout/Reachout.js
+++ src/components/Reachout/Reachout.js
@@ -1,12 +1,13 @@
 import React from "react";
+import './Reachout.css'
 import { InlineWidget } from "react-calendly";
 
-const App = () => {
+const Reachout = () => {
   return (
-    <div className="App">
+    <div className="box-reachout">
       <InlineWidget url="https://calendly.com/maazali1611" />
     </div>
   );
 };
 
-export default App;
+export default Reachout;
